2e44c2f30e82b2df3791c05ca2fed50fd584ba43
mihailpozarski <mihailpozarskirada@10.50.110.28>
2019-01-15 10:29:10 -0300

    eng -> esp

--- lang/es/block_termsandconditions.php
+++ lang/es/block_termsandconditions.php
@@ -1,7 +1,7 @@
 <?php
 $string['pluginname'] = 'Terminos y condiciones';
 $string['title'] = 'Terminos y condiciones';
-$string['headerconfig'] = 'Settings';
-$string['descconfig'] = 'Settings description';
+$string['headerconfig'] = 'Configuración';
+$string['descconfig'] = 'Configuración';
 $string['labelallowrol'] = 'Roles afectados';
 $string['descallowrol'] = 'Roles afectados por el bloque';
