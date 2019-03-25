afc4001554aa45b95816dc6e6f5a5423b0a0b6a5
webcursosti <webcursosti@webcursoss-imac.uai.cl>
2019-03-25 12:34:10 -0300

    add capability

---
+++ db/access.php
@@ -0,0 +1,12 @@
+<?php
+    $capabilities = array(
+    'block/termsandconditions:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
